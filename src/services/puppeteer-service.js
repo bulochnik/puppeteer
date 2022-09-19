1ba2a52624ac901daa478da750e4fac9225cfc02
bulochnik <kukushkaukurushka@gmail.com>
2022-09-19 18:40:02 +0300

    Add heroku manifest

--- src/services/puppeteer-service.js
+++ src/services/puppeteer-service.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer')
 
-const PEPPETEER_DEFAULT_MARGIN = { top: 36, right: 36, bottom: 20, left: 36 }
+const PUPPETEER_DEFAULT_MARGIN = { top: 36, right: 36, bottom: 20, left: 36 }
 
 class PuppeteerService {
   async puppeteer(link) {
@@ -8,7 +8,7 @@ class PuppeteerService {
       const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
       const page = await browser.newPage()
       await page.goto(`${link}`, { waitUntil: 'networkidle2' })
-      const pdf = await page.pdf({ format: 'A4', margin: PEPPETEER_DEFAULT_MARGIN })
+      const pdf = await page.pdf({ format: 'A4', margin: PUPPETEER_DEFAULT_MARGIN })
 
       await browser.close()
       return pdf
